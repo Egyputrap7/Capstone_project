8cb9ca9584fca218e7a7412c08e42f854d62f00e
fitrialfiana <fitrialfiana62@gmail.com>
2023-12-11 15:53:04 +0700

    Ubah warna tombol - page feedback

--- resources/views/main/feedback.blade.php
+++ resources/views/main/feedback.blade.php
@@ -12,12 +12,16 @@
             <form method="post" action="/main/feedback">
                 @csrf
                 <div class="p-3">
-                    <label for="message" class="form-label">Isi Kritikan atau Saran</label>
+                    <label for="message" class="form-label" style="font-weight: bold; color: black; font-size: 14pt;">Isi Kritikan atau Saran</label>
                     <textarea class="form-control" id="pesan" name="pesan" rows="12" required></textarea>
                 </div>
-                <div class="d-grid gap-2 p-5">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                 <!--<div class="d-grid gap-1 p-2">
+                <button type="submit" class="btn btn-primary" style="background-color: #04988F; width: 2cm;">Submit</button>
+                </div>-->
+                <div class="d-grid gap-1 p-2" style="bottom: 0; right: 0; margin-bottom: 20px; margin-right: 10px;">
+                    <button type="submit" class="btn btn-primary" style="background-color: #04988F; width: 2cm;">Submit</button>
                 </div>
+
             </form>
         </div>
     </div>
